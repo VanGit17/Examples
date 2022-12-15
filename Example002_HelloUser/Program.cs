c1c8af67a7e3b3c9e1fa91d8a7338f626f07df3b
Van <bigfilessender@mail.ru>
2022-12-15 19:19:40 +0300

    проект 15-12

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("введите ваше имя");;
+        string username = Console.ReadLine();
+Console.WriteLine("Привет,");
+Console.WriteLine(username);
+    }
+}
